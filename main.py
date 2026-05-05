a7d4c2b762d84351fb809237d6141309a4a91f69
hectorlequen <hector.lequen@gmail.com>
2026-05-05 13:11:47 +0200

    MODIFY - Remove Black formatter to use ruff and add precommit

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 import pandas as pd
+
 from utils.cleaning import (
     clean_data,
-    remove_spaces,
-    manage_none_nan,
-    format_emails,
-    add_valid_mail_column,
-    capitalize_value,
 )
-
 
 df = pd.read_csv("data/input.csv")
 
 print(df)
 
+
 df = clean_data(df)
-
-
-def test(a, b):
-    print(a + b)
-
-
-def test(a, b):
-    print(a + b)
 
 
 print(df)
--- utils/cleaning.py
+++ utils/cleaning.py
@@ -1,5 +1,6 @@
-from email_validator import validate_email, EmailNotValidError
 import pandas as pd
+from email_validator import EmailNotValidError, validate_email
+
 
 def clean_data(df):
     df = remove_spaces(df)
@@ -9,6 +10,7 @@ def clean_data(df):
         df[col] = df[col].apply(capitalize_value)
     df = df.drop_duplicates(keep="first")
     return df
+
 
 def is_valid_email(email: str) -> bool:
     try:
@@ -25,29 +27,31 @@ def add_valid_mail_column(df):
             _l.append(is_valid_email(email))
         except TypeError:
             _l.append(False)
-            
+
     df.insert(3, "is_email_valid", _l)
-    df_unvalid_emails = df.loc[df['is_email_valid'] == False]
-    
-    
+    # df_unvalid_emails = df.loc[not df["is_email_valid"]]
+
 
 def remove_spaces(df):
     def clean_strings(x):
         if isinstance(x, str):
             return x.strip()
         return x
+
     df = df.map(clean_strings)
     return df
+
 
 def manage_none_nan(df):
     df = df.replace({"": None})
     df = df.where(pd.notnull(df), None)
     return df
-    
+
+
 def format_emails(df):
     df["email"] = df["email"].apply(lambda x: x.lower() if x else x)
     return df
-    
+
+
 def capitalize_value(x):
     return x.capitalize() if x else x
-    
